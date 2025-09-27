4f2998601770588aaf5b8e7d1a781a8c5904416d
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2025-09-27 21:41:29 +0900

    Open the appropriate page to reserve a program

--- bin/reserve.ts
+++ bin/reserve.ts
@@ -7,7 +7,7 @@ import { chromium } from "playwright";
 const { values: {
   'user-data-dir': userDataDir,
   'exec-path': executablePath,
-}} = parseArgs({
+} } = parseArgs({
   options: {
     'user-data-dir': {
       type: 'string',
@@ -18,27 +18,27 @@ const { values: {
   },
 });
 
-// if (!userDataDir || !statSync(userDataDir).isDirectory()) {
-//   throw new Error('--user-data-dir must be a directory path');
-// }
-// console.debug(userDataDir);
+if (!userDataDir || !statSync(userDataDir).isDirectory()) {
+  throw new Error('--user-data-dir must be a directory path');
+}
+console.debug(userDataDir);
 
-await (async () => {
+(async () => {
   console.debug('100');
   const browser = await chromium.launch({
     executablePath,
     headless: false,
   });
   console.debug(`150`);
-  const ctx = await browser.newContext();
-  // const ctx = await chromium.launchPersistentContext(userDataDir, {headless: false});
+  // const ctx = await browser.newContext();
+  const ctx = await chromium.launchPersistentContext(userDataDir, { headless: false });
   console.debug(`200`);
   const page = await ctx.newPage();
   console.debug(`300`);
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://live.nicovideo.jp/create');
   console.debug(`400`);
+  await page.getByText('詳細設定を開く').click();
+  console.debug(`500`);
   await ctx.close();
   await browser.close();
 })();
-console.debug('end');
-
