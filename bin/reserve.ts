42711bbb5b9920394368872355d5d07b0bd6404d
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2025-09-27 21:43:14 +0900

    Fix

--- bin/reserve.ts
+++ bin/reserve.ts
@@ -25,20 +25,27 @@ console.debug(userDataDir);
 
 (async () => {
   console.debug('100');
-  const browser = await chromium.launch({
+  // const browser = await chromium.launch({
+  //   executablePath,
+  //   headless: false,
+  // });
+  // console.debug(`150`);
+  // const ctx = await browser.newContext();
+
+  const ctx = await chromium.launchPersistentContext(userDataDir, {
     executablePath,
     headless: false,
   });
-  console.debug(`150`);
-  // const ctx = await browser.newContext();
-  const ctx = await chromium.launchPersistentContext(userDataDir, { headless: false });
   console.debug(`200`);
+
   const page = await ctx.newPage();
   console.debug(`300`);
   await page.goto('https://live.nicovideo.jp/create');
   console.debug(`400`);
   await page.getByText('詳細設定を開く').click();
   console.debug(`500`);
+
   await ctx.close();
-  await browser.close();
+
+  // await browser.close();
 })();
