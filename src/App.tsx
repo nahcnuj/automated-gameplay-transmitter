a9327e2758b49ac1e94922981f24153c57595790
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2025-10-01 17:06:21 +0900

    Use the same font for Latin and CJK characters

--- src/App.tsx
+++ src/App.tsx
@@ -50,7 +50,7 @@ export function App() {
   const numUserComments = comments.filter(({ data: { no, origin } }) => (origin as any)?.meta?.origin?.chat?.liveId === liveId && no).length;
 
   return (
-    <div className="w-screen h-screen max-w-[1280px] max-h-[720px] m-auto overflow-hidden flex">
+    <div className="w-screen h-screen max-w-[1280px] max-h-[720px] m-auto overflow-hidden flex font-[Noto_Sans_CJK_JP]">
       <div className="h-full flex-auto">
         <div className="h-full flex flex-col">
           <div className="flex-auto">
@@ -58,7 +58,7 @@ export function App() {
           </div>
           <div className="h-fit flex-none">
             <div className="after:block after:bg-[#001100f7] after:-mb-10 after:translate-x-243 after:-translate-y-18 after:w-10 after:h-10 after:[clip-path:polygon(100%_50%,8%_0,8%_100%)]">
-              <div className="w-full h-30 p-2 border-5 border-green-200 bg-[#001100f7] rounded-xl text-3xl/12 font-bold text-white [text-shadow:-2px_-2px_4px_#000,2px_2px_4px_#000]">
+              <div className="w-full h-30 p-2 border-5 border-green-200 bg-[#001100f7] rounded-xl text-3xl/10 font-bold text-white [text-shadow:-2px_-2px_4px_#000,2px_2px_4px_#000]">
                 {speechText}
               </div>
             </div>
