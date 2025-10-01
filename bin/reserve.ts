c6c8ec73bbc52e412b61fef5428391108b7a11bd
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2025-10-01 22:04:09 +0900

    Fix the way to get select element

--- bin/reserve.ts
+++ bin/reserve.ts
@@ -85,7 +85,8 @@ if (!userDataDir || !statSync(userDataDir).isDirectory()) {
 
     const children = page.getByText(/20\d\d\/1?\d\/1?\d\([月火水木金土日]\)/);
     console.debug(`624 ${await children.allTextContents()}`);
-    const reserveDate = page.getByRole('listbox').filter({ has: children });
+    console.debug(`    ${await page.getByRole('listbox').allTextContents()}\n    ${await page.locator('select').allTextContents()}\n   ${await page.locator('select', { has: children }).allTextContents()}`);
+    const reserveDate = page.locator('select').filter({ has: children });
     await reserveDate.click();
     console.debug(`625`);
     const label = await children.nth(3).textContent() ?? '(null)';
