6c5494d05f5d81e6e005c14f51b7c9831a5fe163
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2025-10-01 18:41:27 +0900

    Let user log in to get a session

--- bin/reserve.ts
+++ bin/reserve.ts
@@ -18,43 +18,44 @@ const { values: {
   },
 });
 
-if (!userDataDir || !statSync(userDataDir).isDirectory()) {
-  throw new Error('--user-data-dir must be a directory path');
-}
-console.debug(userDataDir);
+// if (!userDataDir || !statSync(userDataDir).isDirectory()) {
+//   throw new Error('--user-data-dir must be a directory path');
+// }
+// console.debug(userDataDir);
 
-const url = await fetch('http://localhost:9222/json/version').then((res) => res.json()).then(({ webSocketDebuggerUrl }) => webSocketDebuggerUrl);
+// const url = await fetch('http://localhost:9222/json/version').then((res) => res.json()).then(({ webSocketDebuggerUrl }) => webSocketDebuggerUrl);
 
 (async () => {
-  console.debug('100');
-  // const browser = await chromium.launch({
-  //   executablePath,
-  //   headless: false,
-  // });
-  // console.debug(`150`);
-  // const ctx = await browser.newContext();
+  console.debug(`0`);
 
-  const browser = await chromium.connectOverCDP('http://localhost:9222/', { timeout: 0 });
-  console.debug('150');
-  const ctx = browser.contexts()[0];
+  const browser = await chromium.launch({
+    executablePath,
+    headless: false,
+  });
+  console.debug(`100`);
+
+  const ctx = await browser.newContext();
   if (!ctx) {
     throw new Error('could not get a context of the browser');
   }
-  // const ctx = await chromium.launchPersistentContext(userDataDir, {
-  //   executablePath,
-  //   args: ['--no-sandbox'],
-  //   headless: false,
-  // });
-  console.debug(`200`);
+  console.debug('200');
 
   const page = await ctx.newPage();
   console.debug(`300`);
+
   await page.goto('https://live.nicovideo.jp/create');
   console.debug(`400`);
-  await page.getByText('詳細設定を開く').click();
+
+  const btn = page.locator('button', { hasText: '詳細設定を開く' });
+  console.debug(`450`);
+  await btn.waitFor({ state: 'visible', timeout: 600_000 });
   console.debug(`500`);
 
-  await ctx.close();
+  await btn.click();
+  console.debug(`600`);
 
-  // await browser.close();
+  await ctx.close();
+  console.debug(`999`);
+
+  await browser.close();
 })();
