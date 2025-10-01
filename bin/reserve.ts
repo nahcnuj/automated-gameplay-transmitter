5e72f8c406790ab18f214717eb2e5caa9dc588be
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2025-10-01 21:32:25 +0900

    Fix method

--- bin/reserve.ts
+++ bin/reserve.ts
@@ -76,9 +76,11 @@ if (!userDataDir || !statSync(userDataDir).isDirectory()) {
   }
 
   {
-    const reserveCheckbox = page.getByRole('button'/*'checkbox'*/, { name: '予約放送を利用する' });
+    // const reserveCheckbox = page.getByRole('checkbox', { name: '予約放送を利用する' });
+    const reserveCheckbox = page.getByRole('button', { name: '予約放送を利用する' });
     console.debug(`620`);
-    await reserveCheckbox.check();
+    // await reserveCheckbox.check();
+    await reserveCheckbox.click();
     console.debug(`621`);
 
     const reserveDate = page.getByPlaceholder(/20\d\d\/1?\d\/1?\d\([月火水木金土日]\)/);
