8f2b685288aaefe9b2c1a4028a88ff099ffdb4fe
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2025-10-04 17:45:16 +0900

    Add minimal script to play Cookie Clicker automatically

---
+++ bin/cookieclicker.ts
@@ -0,0 +1,185 @@
+#!/usr/bin/env bun
+
+import { readFileSync, statSync, writeFileSync } from "node:fs";
+import { parseArgs } from "node:util";
+import { chromium } from "playwright";
+
+const { values: {
+  file,
+  browser: executablePath,
+} } = parseArgs({
+  options: {
+    file: {
+      short: 'f',
+      type: 'string',
+      default: './var/cookieclicker.txt',
+    },
+    browser: {
+      type: 'string',
+      default: '/usr/bin/chromium',
+    },
+  },
+});
+
+const browser = await chromium.launch({
+  executablePath,
+  headless: false,
+  args: [
+    '--window-size=960,576',
+    '--window-position=1280,300',
+  ],
+  slowMo: 100,
+});
+
+const ctx = await browser.newContext();
+if (!ctx) {
+  await browser.close();
+  throw new Error('could not create a context');
+}
+
+const page = await ctx.newPage();
+if (!page) {
+  await ctx.close();
+  await browser.close();
+  throw new Error('could not create a page');
+}
+
+await page.goto('https://orteil.dashnet.org/cookieclicker/');
+
+try {
+  await page.getByText('Got it').click();
+  console.debug(`Got it!`);
+} catch {
+  // do nothing
+}
+
+try {
+  await page.getByText('日本語').click();
+  console.debug(`日本語`);
+} catch {
+  // do nothing
+}
+
+try {
+  const btn = page.getByText('次回から表示しない');
+  await btn.waitFor({ state: 'visible', timeout: 10_000 });
+  await btn.click();
+  console.debug(`Do not show again`);
+} catch {
+  // do nothing
+}
+
+{
+  console.debug(`Configurating...`);
+
+  const options = page.locator('.subButton', { hasText: 'オプション' });
+  await options.click();
+  console.debug(`Clicked the option button.`);
+
+  const menu = page.locator('#menu');
+
+  await menu.locator('#volumeSlider').fill('100');
+  console.debug(`Made the volume maximum.`);
+
+  try {
+    await menu.getByText(/高品質.*ON/i).click();
+    console.debug(`Turned off high quality.`);
+  } catch {
+    // do nothing
+  }
+
+  try {
+    await menu.getByText(/CSS.*ON/i).click();
+    console.debug(`Turned off CSS filters.`);
+  } catch {
+    // do nothing
+  }
+
+  try {
+    await menu.getByText(/パーティクル.*ON/i).click();
+    console.debug(`Turned off particles.`);
+  } catch {
+    // do nothing
+  }
+
+  await menu.locator('.close').click();
+  console.debug(`Closed the menu.`);
+}
+
+let data: string | undefined;
+try {
+  data = readFileSync(file, 'utf8');
+} catch (err) {
+  console.warn(err);
+}
+
+if (data) {
+  console.debug(`Importing...`);
+
+  const options = page.locator('.subButton', { hasText: 'オプション' });
+  await options.click();
+  console.debug(`Clicked the option button.`);
+
+  const menu = page.locator('#menu');
+
+  await menu.press('Control+O');
+  console.debug(`Pressed Ctrl+O.`);
+
+  const popup = page.locator('#prompt');
+
+  await popup.getByRole('textbox').fill(data);
+  await popup.getByText('ロード').click();
+
+  await popup.waitFor({ state: 'hidden' });
+  console.debug(`Imported!`);
+
+  await menu.locator('.close').click();
+  console.debug(`Closed the menu.`);
+}
+
+const clicker = setInterval(async () => {
+  const bigCookie = page.locator('#bigCookie');
+  try {
+    await bigCookie.click({ timeout: 100 });
+  } catch {
+    // do nothing
+  }
+}, 250);
+
+const exporter = setInterval(async () => {
+  console.debug(`Exporting...`);
+  try {
+    const options = page.locator('.subButton', { hasText: 'オプション' });
+    await options.click();
+    console.debug(`Clicked the option button.`);
+
+    const menu = page.locator('#menu');
+    await menu.getByText('エクスポート').click();
+    console.debug(`Clicked the exporting button`);
+
+    const popup = page.locator('#prompt');
+    console.debug(`Popped up the exporting menu.`);
+
+    data = await popup.getByRole('textbox').inputValue();
+    writeFileSync(file, data, 'utf8');
+
+    await popup.getByText('完了').click();
+    await popup.waitFor({ state: 'hidden' });
+    console.debug(`Exported!`);
+
+    await menu.locator('.close').click();
+    console.debug(`Closed the menu.`);
+  } catch (err) {
+    console.warn('Failed to save.', err);
+  }
+}, 600_000);
+
+// const notes = page.locator('#notes');
+// notes.dispatchEvent()
+
+setTimeout(async () => {
+  clearInterval(clicker);
+  clearInterval(exporter);
+  await ctx.close();
+  await browser.close();
+}, 6_000_000);
