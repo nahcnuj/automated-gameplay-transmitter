4cff1844a0d6d5097b0d0f9cc5088261b79d6741
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2025-10-05 15:51:50 +0900

    Redesign comment list

--- src/components/CommentList.tsx
+++ src/components/CommentList.tsx
@@ -9,8 +9,8 @@ export function CommentList({ comments }: { comments: NicoNamaComment[] }) {
   return (
     <div className="w-full h-full flex flex-col justify-end">
       {comments.map(({ data }) => (
-        <div key={data.id} className={`p-1 my-1 rounded-md border-1 border-[#fbf0df] relative ${isUser(data) ? 'bg-[#001100f7]' : "bg-blue-950/95 after:[content:'〟'] after:absolute after:bottom-1 after:-right-6 after:text-5xl after:text-blue-50/20"}`}>
-          <div className={`text-lg/6 ${fromCruise(data) ? '' : 'font-bold'}`}>
+        <div key={data.id} className={`p-1 my-1 rounded-md border-1 border-[#fbf0df] relative ${isUser(data) ? 'bg-[#001100f7]' : "bg-[#000011f7] after:[content:'〟'] after:absolute after:bottom-1 after:-right-6 after:text-5xl after:text-blue-50/20"}`}>
+          <div className={fromCruise(data) ? 'text-md/6' : 'text-lg/6 font-bold'}>
             {data.comment}
           </div>
         </div>
