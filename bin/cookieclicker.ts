9b3792b0181d07fd17ed3d951d60697b4f25fcc1
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2025-11-18 20:29:27 +0900

    Fix

--- bin/cookieclicker.ts
+++ bin/cookieclicker.ts
@@ -391,7 +391,7 @@ const config = {
 let exitCode = 0;
 
 const msPerTick = 1_000;
-const ticksToStats = Math.floor(2_000_000 / msPerTick);
+const ticksToStats = Math.floor(1_000_000 / msPerTick);
 const ticksToSave = Math.floor(600_000 / msPerTick);
 
 const timeoutMs = 600_000_000;
