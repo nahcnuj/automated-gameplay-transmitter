b7be6a93bbd971ee39df6120fc8d2b826a6a1681
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2025-11-22 14:13:44 +0900

    Create Browser interface

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
 export { Box } from "./src/components/Box";
 export { Container } from "./src/components/Container";
 export { Layout } from "./src/components/Layout";
+
+export { createSocketPair } from "./src/lib/socket";
+
+export type { Browser } from "./src/lib/Browser";
+
