2816f66f8dcf80e5ad2a0071d04f143d1f30c861
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2025-11-23 20:08:19 +0900

    Add page

--- src/lib/Browser.ts
+++ src/lib/Browser.ts
@@ -1,4 +1,4 @@
-import type { Locator } from "playwright"
+import type { Locator, Page } from "playwright"
 
 export interface Browser {
   open(url: string): Promise<void>
@@ -11,4 +11,5 @@ export interface Browser {
   fillByRole(value: string, role: Parameters<Locator['getByRole']>[0], selector: string): Promise<void>
 
   get url(): string
+  get page(): Page
 };
