6e21174e37c120b69ce7a65998be4c432adadfb0
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2025-11-23 16:23:24 +0900

    Add some methods

--- src/lib/Browser.ts
+++ src/lib/Browser.ts
@@ -1,8 +1,14 @@
+import type { Locator } from "playwright"
+
 export interface Browser {
   open(url: string): Promise<void>
   close(): Promise<void>
 
   clickByText(text: string): Promise<void>
+
+  press(key: string, selector: string): Promise<void>
+
+  fillByRole(value: string, role: Parameters<Locator['getByRole']>[0], selector: string): Promise<void>
 
   get url(): string
 };
