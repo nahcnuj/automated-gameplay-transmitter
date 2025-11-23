8408882a138aa76a69d04eeccca2363670c58fce
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2025-11-23 20:44:13 +0900

    Fix

--- src/lib/Browser.ts
+++ src/lib/Browser.ts
@@ -1,5 +1,3 @@
-import type { Locator, Page } from "playwright"
-
 export interface Browser {
   open(url: string): Promise<void>
   close(): Promise<void>
@@ -8,7 +6,7 @@ export interface Browser {
 
   press(key: string, selector: string): Promise<void>
 
-  fillByRole(value: string, role: Parameters<Locator['getByRole']>[0], selector: string): Promise<void>
+  fillByRole(value: string, role: string, selector: string): Promise<void>
 
   get url(): string
   get document(): Document
