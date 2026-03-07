7b2214fc1cbd5f32d001a83ba8727ef73a66ea6e
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2026-03-07 15:49:11 +0900

    chore: update dependencies and tests

--- src/components/HighlightOnChange.test.tsx
+++ src/components/HighlightOnChange.test.tsx
@@ -1,6 +1,8 @@
 import { renderToString } from "react-dom/server";
 import { HighlightOnChange } from "./HighlightOnChange";
 
+jest.useFakeTimers();
+
 test("HighlightOnChange renders children", () => {
   const html = renderToString(<HighlightOnChange timeout={100}>hi</HighlightOnChange>);
   expect(html).toContain("hi");
---
+++ src/hooks/useInterval.test.tsx
@@ -0,0 +1,6 @@
+import { useInterval } from "./useInterval";
+
+test("useInterval is a function", () => {
+  expect(typeof useInterval).toBe("function");
+});
+
