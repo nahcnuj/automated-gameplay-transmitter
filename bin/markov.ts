8414e5dc8a743204cd2cedd3ca9d7119c96d5215
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2026-03-28 21:18:10 +0900

    chore(markov-cli): use top-level await in bin/markov.ts

--- bin/markov.ts
+++ bin/markov.ts
@@ -1,6 +1,4 @@
 #!/usr/bin/env bun
 import { parseAndGetCommand } from '../src/lib/MarkovModel/cli.ts';
 
-;(async () => {
-  await parseAndGetCommand(process.argv.slice(2));
-})();
+await parseAndGetCommand(process.argv.slice(2));
