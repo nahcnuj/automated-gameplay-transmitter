2981dbde09cdd726e3d7985aaecfea3ca33469cc
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2026-03-29 09:33:42 +0900

    chore(cli): import from fs/promises instead of fs.promises

--- src/lib/MarkovModel/cli.ts
+++ src/lib/MarkovModel/cli.ts
@@ -1,4 +1,4 @@
-import { promises as fs } from 'fs';
+import fs from 'fs/promises';
 import path from 'path';
 import { parseArgs } from 'util';
 import type { MarkovModelData, WeightedCandidates } from './MarkovModel';
